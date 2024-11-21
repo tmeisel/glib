7f68471054d04cea4742c732f96891291bc9eb92
Tobias Meisel <tobias.meisel@t-systems.com>
2024-11-21 23:33:10 +0100

    error: added more codes

--- error/code.go
+++ error/code.go
@@ -3,14 +3,16 @@ package error
 type Code int
 
 const (
-	CodeUser         Code = 40000
-	CodeAuthRequired Code = 40100
-	CodeForbidden    Code = 40300
-	CodeNotFound     Code = 40400
-	CodeConflict     Code = 40900
-	CodeDuplicateKey Code = 40901
-
-	CodeInternal Code = 50000
+	CodeUser               Code = 40000
+	CodeAuthRequired       Code = 40100
+	CodeForbidden          Code = 40300
+	CodeNotFound           Code = 40400
+	CodeConflict           Code = 40900
+	CodeDuplicateKey       Code = 40901
+	CodePreconditionFailed Code = 41200
+	CodeGone               Code = 41000
+	CodeTooManyRequests    Code = 42900
+	CodeInternal           Code = 50000
 )
 
 func (c Code) String() string {
