2da606d59271e24833cc88b7239d7e3cd2931c67
Tobias Meisel <tobias.meisel@t-systems.com>
2024-11-21 18:31:40 +0100

    utils: added strutils.(Must)Random

--- utils/strutils/strutils.go
+++ utils/strutils/strutils.go
@@ -1,5 +1,12 @@
 package strutils
 
+import (
+	"crypto/rand"
+	"math/big"
+	"strings"
+)
+
+// Ptr returns a pointer to s
 func Ptr(s string) *string {
 	return &s
 }
@@ -23,4 +30,48 @@ func SubString(s string, pos, length int) string {
 	}
 
 	return string(runes[pos:l])
+}
+
+const (
+	// AlphabetUCChars Upper case chars
+	AlphabetUCChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	// AlphabetLCChars // Lower case chars
+	AlphabetLCChars      = "abcdefghijklmnopqrstuvwxyz"
+	AlphabetNumbers      = "1234567890"
+	AlphabetSpecialChars = ",;.:-_!§$%&/()=?*+#"
+
+	// Alphabet is a combination of upper and lower case chars, numbers and special chars
+	Alphabet = AlphabetLCChars +
+		AlphabetUCChars +
+		AlphabetNumbers +
+		AlphabetSpecialChars
+
+	// AlphabetReadable is an alphanumeric alphabet that excludes chars that can
+	// be confused, depending on the used font. E.g. 0 and O or l and I
+	AlphabetReadable = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKKLMNPQRSTUVWXYZ2345689"
+)
+
+// Random returns a random string generated using crypto/rand.
+// The alphabet can be one or more strings, including the ones
+// provided by this pkg like AlphabetNumbers.
+func Random(length int, alphabet ...string) (string, error) {
+	chars := strings.Join(alphabet, "")
+	output := make([]byte, length)
+	for i := 0; i < length; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			return "", err
+		}
+
+		output[i] = chars[num.Int64()]
+	}
+
+	return string(output), nil
+}
+
+// MustRandom returns a random string. For a detailed description,
+// see Random
+func MustRandom(length int, alphabet string) string {
+	str, _ := Random(length, alphabet)
+	return str
 }
--- utils/strutils/strutils_test.go
+++ utils/strutils/strutils_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSubString(t *testing.T) {
@@ -46,4 +47,54 @@ func TestSubString(t *testing.T) {
 			assert.Equal(t, tc.Substr, SubString(input, tc.Pos, tc.Len))
 		})
 	}
+}
+
+func TestRandom(t *testing.T) {
+	type testCase struct {
+		expectedLength   int
+		expectedAlphabet []string
+	}
+
+	for name, tc := range map[string]testCase{
+		"lower case": {
+			expectedLength: 10,
+			expectedAlphabet: []string{
+				AlphabetLCChars,
+			},
+		},
+		"combined": {
+			expectedLength: 30,
+			expectedAlphabet: []string{
+				AlphabetLCChars,
+				AlphabetNumbers,
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			output, err := Random(tc.expectedLength, tc.expectedAlphabet...)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.expectedLength, len(output))
+
+		charLoop:
+			for _, char := range output {
+				for _, alphabet := range tc.expectedAlphabet {
+					for _, alpha := range alphabet {
+						if char == alpha {
+							continue charLoop
+						}
+					}
+				}
+
+				t.Errorf("unexpected character '%c'", char)
+			}
+		})
+	}
+}
+
+func TestMustRandom(t *testing.T) {
+	assert.NotPanics(t, func() {
+		out := MustRandom(10, AlphabetNumbers)
+		assert.Equal(t, 10, len(out))
+	})
 }
