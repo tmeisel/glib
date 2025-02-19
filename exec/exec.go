8c8b56432fced26afc473eb77352c0edd824e203
Tobias Meisel <tobias.meisel@t-systems.com>
2025-02-19 12:47:30 +0100

    exec: Added SigtermHandler

--- exec/exec.go
+++ exec/exec.go
@@ -1,8 +1,27 @@
 package exec
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	ctxPkg "github.com/tmeisel/glib/ctx"
 )
+
+func SigtermHandler(ctx context.Context, cancelFn context.CancelFunc) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-sigs
+
+	if logger := ctxPkg.GetLogger(ctx); logger != nil {
+		logger.Infof(ctx, "received %s, shutting down gracefully", sig.String())
+	}
+
+	cancelFn()
+}
 
 func Deferred(fn func() error) {
 	if err := fn(); err != nil {
